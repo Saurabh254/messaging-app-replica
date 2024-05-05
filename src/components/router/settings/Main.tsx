b8f17c7f6a0b81c78204b309748bb1f2e23dacc3
Saurabh <75117608+Saurabh254@users.noreply.github.com>
2024-05-05 17:49:55 +0530

    Added session timeout functionality (#3)

--- src/components/router/settings/Main.tsx
+++ src/components/router/settings/Main.tsx
@@ -2,9 +2,9 @@ import { colors } from "../../../colors";
 import { Link } from "react-router-dom";
 import { IoMdArrowBack } from "react-icons/io";
 import Profiles from "./Profiles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DevicesComponent from "./Devices";
-
+import { useNavigate } from "react-router-dom";
 // 0 -> Profiles
 // 1 -> Privacy and Safety
 // 2 -> Devices
@@ -17,9 +17,17 @@ import DevicesComponent from "./Devices";
 
 const SettingsPage = ({ loggedInUser }) => {
     const [settingState, setSettingState] = useState('profiles')
+    const navigate = useNavigate();
+    useEffect(() => {
+        const token = localStorage.getItem('access_token');
+        if (token == null) {
+            navigate('/login')
+        }
+
+    })
     return (
         <div className="flex justify-between w-full h-full gap-8">
-            <div className="flex flex-col items-start w-1/6 [&>*]:px-4 [&>*]:py-2 min-w-[250px] [&>*]:w-full text-center text-white" style={{ backgroundColor: colors.secondary }} onClick={(e) => e.target.classList[0] !== settingState ? setSettingState(e.target.classList[0]) : null} >
+            <div className="flex flex-col items-start w-1/6 [&>*]:px-4 [&>*]:py-2 min-w-[250px] [&>*]:w-full text-center text-white" style={{ backgroundColor: colors.secondary }} onClick={(e) => e.target.classList[0] !== settingState ? setSettingState(e.target?.classList[0]) : null} >
                 <div className="w-full my-2"><Link to='/'><IoMdArrowBack className='text-2xl' /></Link></div>
                 <span className=" border-b-2 mb-2 font-semibold">User Settings</span>
                 <button className='profiles'>Profiles</button>
@@ -28,7 +36,10 @@ const SettingsPage = ({ loggedInUser }) => {
                 <button className='account' >My Account</button>
                 <button className="advanced hover:text-gray-400">Advanced</button>
                 <button className="help hover:text-gray-400">Help</button>
-                <button className='logout' >Log Out</button>
+                <button className='logout' onClick={() => {
+                    localStorage.removeItem('access_token')
+                    navigate('/login')
+                }} >Log Out</button>
             </div>
             <div className="w-5/6 h-full items-center flex flex-col p-8">
                 {settingState == 'profiles' ? <Profiles /> : ""}
