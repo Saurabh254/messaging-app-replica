c3b3904b739278ce0baad137132824a8f358bf41
Saurabh <sauravvishwakarma030@gmail.com>
2024-09-02 00:46:52 +0530

    feat: Added new style login page

--- src/components/router/loginScreen/loginScreen.tsx
+++ src/components/router/loginScreen/loginScreen.tsx
@@ -1,65 +1,67 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { colors } from "../../../colors";
 
 const LoginScreen = () => {
-    const navigate = useNavigate();
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [otp, setOTP] = useState('');
-    const [verificationState, setVerificationState] = useState(0);
-    useEffect(() => {
-        setTimeout(() => {
-            localStorage.removeItem('access_token')
-            const token = localStorage.getItem('access_token')
-            if (token == null) {
-                navigate('/login')
-            }
+  return (
+    <React.Fragment>
+      <div className="flex items-center justify-center h-full w-full rounded-xl overflow-hidden px-8">
+        <div className=" rounded-xl sm:bg-gray-100 p-8 sm:shadow-md h-3/4 w-[400px] items-center justify-center flex flex-col">
+          <span className="font-semibold text-xl md:hidden">
+            Messaging App Replica
+          </span>
+          <hr className="border-1 border-indigo-500 w-[80%] mt-4 md:hidden" />
+          <br />
+          <div>
+            <div className="flex flex-col p-2 px-8">
+              <span className="text-sm px-2 mb-2">Enter your username</span>
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="username"
+                className="border-2 p-2 rounded-xl mb-4"
+              />
 
-        }, 100000)
-    })
-    return (
-        <div className="flex flex-col items-center justify-center h-[100svh] w-[100svw]">
-            <div className=" p-24 rounded-xl shadow-xl flex flex-col gap-16" style={{ backgroundColor: colors.secondary }}>
-
-                <div className="flex items-center w-full justify-center">
-
-                    <span className="text-3xl text-white font-bold">Messenger Desktop</span>
-                </div>
-                {verificationState == 0 ?
-                    <div className="flex flex-col gap-4 text-white font-bold">
-                        <span className="text-xl">Phone Number:</span>
-                        <input type="text" name="" id="" value={phoneNumber} onInput={(e) => { setPhoneNumber(e.target.value) }} className="text-2xl text-black py-3 px-4 font-normal rounded-lg shadow-lg" />
-                        <span className="ml-auto cursor-pointer">need help?</span>
-                    </div> : <div className="flex flex-col gap-4 text-white font-bold items-center">
-                        <span className="text-xl">Enter the OTP:</span>
-                        <input type="text" name="" id="" value={otp} onInput={(e) => { setOTP(e.target.value) }} className="text-2xl text-black py-3 px-4 font-normal rounded-lg shadow-lg tracking-wide w-[75%]" maxLength={6} minLength={6} />
-                        <span className="ml-auto cursor-pointer mr-12">need help?</span></div>
-                }
-                <div className="w-full flex">
-                    <input type="button" value={verificationState == 0 ? "Send OTP" : "Verify"} onClick={async function () {
-                        if (verificationState == 0) {
-                            const response = await fetch('http://127.0.0.1:8000/ha', { method: "POST", })
-                            if (response.status != 200) { return }
-                            const response_data = await response.json()
-                            console.log(response_data)
-                            setVerificationState(1)
-                        } else {
-                            const response = await fetch('http://127.0.0.1:8000/ha', { method: "POST", })
-                            if (response.status != 200) { return }
-                            const response_data = await response.json()
-                            console.log(response_data)
-                            localStorage.setItem('access_token', "some_secret_token")
-                            navigate('/')
-
-                        }
-                    }
-                    } className="bg-white p-4 rounded-lg shadow-lg ml-auto mr-4 disabled:bg-slate-400 hover:cursor-pointer disabled:hover:scale-100 disabled:text-slate-600
-                    hover:scale-105" disabled={phoneNumber.length == 10 ? false : true} />
-                </div>
+              <span className="text-sm px-2 mb-2">Enter your password</span>
+              <input
+                type="password"
+                name=""
+                id=""
+                placeholder="password"
+                className="border-2 p-2 rounded-xl"
+              />
+              <span className="cursor-pointer text-xs text-indigo-500 font-semibold text-right px-4 py-2">
+                forgot password?
+              </span>
             </div>
-
+            <div className="flex-col flex justify-evenly mt-1 gap-4">
+              <button
+                type="submit"
+                className="bg-indigo-500 text-white mx-12 py-1 px-4 rounded-xl shadow-sm hover:scale-105"
+              >
+                Login
+              </button>
+              <span className="text-sm w-full text-center ">
+                new user?&nbsp;
+                <span className="text-indigo-500 font-semibold cursor-pointer">
+                  create account
+                </span>
+              </span>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="bg-indigo-500 h-3/4 w-[400px] text-white hidden items-center justify-center text-xl rounded-xl shadow-lg flex-col md:flex">
+          <div className="flex flex-col">
+            <span>Sign in into</span>
+            <span className="font-semibold ">Messaging App Replica</span>
+          </div>
+          <br />
+          <span className="ri-building-3-fill text-[5rem] mt-8 "></span>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default LoginScreen;
